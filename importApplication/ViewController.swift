336e2d199dcf2c017fc5431e3f77eaeabf153607
max77p <shanindrakumar@gmail.com>
2019-10-29 20:00:43 -0400

    first commit

---
+++ importApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  importApplication
+//
+//  Created by Shan Indrakumar on 2019-10-29.
+//  Copyright © 2019 CS2680. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
